cc626676ee6522a13e30fc276eeaf9a32388fdd8
Synchronous <SyncMC@users.noreply.github.com>
2017-11-28 19:18:01 -0500

    Add UUID test

---
+++ tests/test_uuids.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+from mojang_api import get_uuid
+
+USERNAME_UUID_PAIRS = [
+    ('Notch', '069a79f444e94726a5befca90e38aaf5'),
+    ('jeb_', '853c80ef3c3749fdaa49938b674adae6'),
+    ('Synchronous', '15fffb7e57c64b70bbc3a42dddaf0f81')
+]
+
+
+def test_uuids():
+    empirical_username_uuid_pairs = [(username_uuid_pair[0], get_uuid(
+        username_uuid_pair[0])['id']) for username_uuid_pair in USERNAME_UUID_PAIRS]
+    assert empirical_username_uuid_pairs == USERNAME_UUID_PAIRS
