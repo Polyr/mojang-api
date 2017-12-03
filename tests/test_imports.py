8dd72fc1e6aafbaaf5becfc1ce6097a6e92b97b8
Synchronous <SyncMC@users.noreply.github.com>
2017-12-03 08:34:13 -0500

    Fix import list

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -4,21 +4,21 @@ from importlib import import_module
 
 PACKAGE = 'mojang_api'
 IMPORTS = [
-    ('authenticate_user', 'servers.authserver')
-    ('change_skin', 'servers.api')
-    ('generate_client_token', 'utils.uuid')
-    ('get_blocked_servers', 'servers.sessionserver')
-    ('get_statistics', 'servers.api')
-    ('get_status', 'servers.status')
-    ('get_user_profile', 'servers.sessionserver')
-    ('get_username_history', 'servers.api')
-    ('get_uuid', 'servers.api')
-    ('get_uuids', 'servers.api')
-    ('invalidate_access_token', 'servers.authserver')
-    ('refresh_access_token', 'servers.authserver')
-    ('reset_skin', 'servers.api')
-    ('signout_user', 'servers.authserver')
-    ('upload_skin', 'servers.api')
+    ('authenticate_user', 'servers.authserver'),
+    ('change_skin', 'servers.api'),
+    ('generate_client_token', 'utils.uuid'),
+    ('get_blocked_servers', 'servers.sessionserver'),
+    ('get_statistics', 'servers.api'),
+    ('get_status', 'servers.status'),
+    ('get_user_profile', 'servers.sessionserver'),
+    ('get_username_history', 'servers.api'),
+    ('get_uuid', 'servers.api'),
+    ('get_uuids', 'servers.api'),
+    ('invalidate_access_token', 'servers.authserver'),
+    ('refresh_access_token', 'servers.authserver'),
+    ('reset_skin', 'servers.api'),
+    ('signout_user', 'servers.authserver'),
+    ('upload_skin', 'servers.api'),
     ('validate_access_token' 'servers.authserver')
 ]
 
