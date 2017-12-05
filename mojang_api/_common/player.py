70489427201b32b639b15c64077a7cd44ad236bc
Synchronous <SyncMC@users.noreply.github.com>
2017-12-05 17:36:33 -0500

    Correct argument slicing

--- mojang_api/_common/player.py
+++ mojang_api/_common/player.py
@@ -6,12 +6,19 @@ from ..user.player import Player
 from ..utils.uuid import is_valid_uuid
 
 
-def accept_player(*arg_pos_slice):
+def accept_player(*arg_pos_range):
+    arg_pos_slice = slice(*arg_pos_range)
+    arg_pos_range = [0 if arg_pos_slice.start is None else arg_pos_slice.start,
+                     arg_pos_slice.stop, 1 if arg_pos_slice.step is None else arg_pos_slice.step]
+
     def decorator(func):
         @wraps(func)
         def with_player_acceptance(*args, **kwargs):
             args = list(args)
-            for arg_pos in range(*arg_pos_slice):
+            if arg_pos_range[1] is None:
+                arg_pos_range[1] = len(args)
+
+            for arg_pos in range(*arg_pos_range):
                 player = args[arg_pos]
                 if isinstance(player, Player):
                     pass
